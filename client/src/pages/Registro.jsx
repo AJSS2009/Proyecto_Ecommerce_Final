a9660945fcee2bfdb0669b1540a1604e2f3dee51
AJSS2009 <ajss34@gmail.com>
2023-09-28 15:40:59 -0300

    Se agrega JS a pagina de Registros para recibir los valores

--- client/src/pages/Registro.jsx
+++ client/src/pages/Registro.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 const Registro = () => {
+
+  const [inputs, setInputs] = useState({
+    nombre: "",
+    apellidos: "",
+    usuarios: "",
+    correo: "",
+    contrasena: "",
+    confirmar_contrasena: "",
+  })
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault ();
+
+    console.log(inputs)
+  }
+
   return (
     <div className="w-full flex justify-center items-center">
-      <form className="bg-white p-4 shadow-md border rounded my-5 py-3">
+      <form className="bg-white p-4 shadow-md border rounded my-5 py-3" onSubmit={submitHandler}>
         <h2 className="text-center w-full p-3 text-grey-500 text-x1 font-bold">Registro de Usuario</h2>
         <div>
           <label className="text-grey-500 mb-2 font-bold" for="Nombre">
@@ -13,6 +37,8 @@ const Registro = () => {
           <input type="text"
             placeholder="Nombre"
             id="nombre"
+            name="nombre"
+            onChange={onChangeHandler}
             className="w-full py-2 px-3 text-grey-500 shadow focus:outline-none focus:shadow-md border border-grey-500 rounded"
           />
         </div>
@@ -24,50 +50,60 @@ const Registro = () => {
           <input type="text"
             placeholder="Apellidos"
             id="apellidos"
+            name="apellidos"
+            onChange={onChangeHandler}
             className="w-full py-2 px-3 text-grey-500 shadow focus:outline-none focus:shadow-md border border-grey-500 rounded"
           />
         </div>
 
         <div>
-          <label className="text-grey-500 mb-2 font-bold" for="usuario">
+          <label className="text-grey-500 mb-2 font-bold" htmlFor="usuario">
             Usuario
           </label>
           <input type="text"
             placeholder="Usuario"
             id="usuario"
+            name="usuario"
+            onChange={onChangeHandler}
             className="w-full py-2 px-3 text-grey-500 shadow focus:outline-none focus:shadow-md border border-grey-500 rounded"
           />
         </div>
 
         <div>
-          <label className="text-grey-500 mb-2 font-bold" for="correo">
+          <label className="text-grey-500 mb-2 font-bold" htmlFor="correo">
             Correo Electronico
           </label>
           <input type="text"
             placeholder="correo@correo.com"
             id="correo"
+            name="correo"
+            onChange={onChangeHandler}
             className="w-full py-2 px-3 text-grey-500 shadow focus:outline-none focus:shadow-md border border-grey-500 rounded"
           />
         </div>
 
         <div className="mb-2">
-          <label className="text-grey-500 mb-2 font-bold" for="contrasena">
+          <label className="text-grey-500 mb-2 font-bold" htmlFor="contrasena">
             Contraseña
           </label>
           <input type="text"
             placeholder="Contraseña"
             id="contrasena"
+            name="contrasena"
+            onChange={onChangeHandler}
             className="w-full py-2 px-3 text-grey-500 shadow focus:outline-none focus:shadow-md border border-grey-500 rounded"
           />
         </div>
 
         <div className="mb-2">
-          <label className="text-grey-500 mb-2 font-bold" for="confirmar_contrasena">
+          <label className="text-grey-500 mb-2 font-bold" htmlFor="confirmar_contrasena">
             Confirmar Contraseña
           </label>
           <input type="text"
             placeholder="Confirmar Contraseña"
             id="confirmar_contrasena"
+            name="confirmar_contrasena"
+            onChange={onChangeHandler}
             className="w-full py-2 px-3 text-grey-500 shadow focus:outline-none focus:shadow-md border border-grey-500 rounded"
           />
         </div>
