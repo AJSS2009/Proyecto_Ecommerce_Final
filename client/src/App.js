7dd732081695df2bc0840dd1abcf6783965b2e7b
AJSS2009 <ajss34@gmail.com>
2023-09-27 22:13:33 -0300

    Mapeo de paginas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Nagvar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import Sesion from "./pages/Sesion";
+import Registro from "./pages/Registro";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/" element={<Sesion />} />
+          <Route path="/" element={<Registro />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/Nagvar.jsx
+++ client/src/components/Nagvar.jsx
@@ -1,15 +1,75 @@
-import React from "react"
+import React from "react";
+import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+
+    const navHandler = () => { setNav(!nav) }
     return (
-        <div className='w-full h-25 gb-[#00af9a] flex justify-between' >
+        <div className='w-full h-25 gb-black flex justify-between item-center' >
             <h1 className="text-white font-bold md:text-4x1 sm:3x1 text-xl p-3">Tienda en Linea</h1>
-            <ul className="flex p-3">
-                <li className="text-white font-bold p-2 hover:bg-[#000300] cursor-pointer">Inicio</li>
-                <li className="text-white font-bold p-2 hover:bg-[#000300] cursor-pointer">Carro de Compras</li>
-                <li className="text-white font-bold p-2 hover:bg-[#000300] cursor-pointer">Sesion</li>
-                <li className="text-white font-bold p-2 hover:bg-[#000300] cursor-pointer">Registrar</li>
+            <ul className="hidden md:flex p-3">
+                <Link to="/">
+                    <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                        Inicio</li>
+                </Link>
+                <Link to="carro">
+                    <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                        Carro de Compras</li>
+                </Link>
+                <Link to="sesion">
+                    <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                        Sesion</li>
+                </Link>
+                <Link to="registro">
+                    <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                        Registrar</li>
+                </Link>
             </ul>
+
+            <div className="md:hidden">
+                {nav ? (
+                    <AiOutlineClose
+                        onClick={navHandler}
+                        className="text-white text-4x1 px-2"
+                    />
+                ) : (
+                    <AiOutlineMenu
+                        onClick={navHandler}
+                        className="text-white text-4x1 px-2"
+                    />
+                )}
+            </div>
+
+
+            <div className={
+                nav
+                    ? `md:hidden fixed top-0 left-0 h-[100%] w-60 bg-black ease-in-out duration-300`
+                    : `hidden`}>
+                <h1 className="text-white font-bold md:text-4x1 sm:3x1 text-xl p-3">Tienda en Linea</h1>
+                <ul className="flex flex-col text-left p-3">
+                    <Link to="/">
+                        {" "}
+                        <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                            Inicio</li>
+                    </Link>
+                    <Link to="carro">
+                        <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                            Carro de Compras</li>
+                    </Link>
+                    <Link to="sesion">
+                        <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                            Sesion</li>
+                    </Link>
+                    <Link to="registro">
+                        <li className="text-white font-bold p-2 hover:bg-[#2C2A2A] cursor-pointer">
+                            Registrar</li>
+                    </Link>
+                </ul>
+            </div>
         </div>
     );
 };
